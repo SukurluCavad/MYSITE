1e887d3caa6e5cd26fe826059d54d336a62ab081
Jsssslsels <sukurlucavad@gmail.com>
2024-07-05 19:45:16 +0400

    MYSITE

---
+++ BizLandMenta/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+builder.Services.AddControllersWithViews();
+
+
+var app = builder.Build();
+
+
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action}/{id?}"
+    );
+
+app.Run();
